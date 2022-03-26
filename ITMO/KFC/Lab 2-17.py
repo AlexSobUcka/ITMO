8afd3a21adfc8b5146f36d0031f2ffd186538c28
AlexSobucka <spopov030999@gmail.com>
2022-03-26 09:19:45 +0300

    change in folders

---
+++ ITMO/KFC/Lab 2-17.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b998d11b306460a0b331c87918e89d85159176456efd9a390e19b97138b23322
+size 373
---
+++ ITMO/KFC/Lab1-17.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:158c4841aa04ec47c8e4926c9327801af73f0225b631ecac17014aa44423de7e
+size 2267
